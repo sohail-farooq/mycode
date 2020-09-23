508d31b5eb733ed119f6a48615742bba61a8d5cc
sohail-farooq <farsohail@hotmail.com>
2020-09-23 20:29:13 +0000

    Learning to parse log files

---
+++ grandma/grandmacode.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+# For every [buscuit] I find in the [room] I'm going to [flog].
+
+room= ["cheetos","buscuit","fork","trash"]
+
+for buscuit in room:
+
+    print(f"for every {buscuit} i find in your room I'm going to flog you!")
+
+
+
+
+
