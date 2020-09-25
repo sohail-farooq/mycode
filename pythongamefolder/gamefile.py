f81f8fb828adf6f8cf9fc0558bc54eab021d2722
sohail-farooq <farsohail@hotmail.com>
2020-09-25 18:28:23 +0000

    Learning to parse log files

---
+++ pythongamefolder/gamefile.py
@@ -0,0 +1,23 @@
+## A dictionary linking a room to other rooms
+
+def showInstructions():
+    print("what is this?")
+
+
+
+
+
+
+
+rooms = {
+
+            'Hall' : { 
+                  'south' : 'Kitchen'
+                },
+
+            'Kitchen' : {
+                  'north' : 'Hall'
+                }
+
+         }
+
